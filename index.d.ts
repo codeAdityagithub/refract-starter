7fd30650eafbb71f94347c49bd4bba2c1d797b74
Aditya <adityathebest2019@gmail.com>
2025-03-10 17:23:04 +0530

    updated to the new version 1.1.3- support complete intellisense

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,3 @@
-declare const FRAGMENT = "FRAGMENT";
-
-declare namespace JSX {
-    interface IntrinsicElements {
-        [elemName: string]: any; // Support all standard HTML tags
-    }
-}
-
 declare module "*.module.css" {
     const classes: { [key: string]: string };
     export default classes;
